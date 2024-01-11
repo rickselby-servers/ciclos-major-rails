b0fbe0aa7dba9174dc6f21d3da9e20c915d40728
Rick Selby <rick@selby-family.co.uk>
2024-01-11 10:16:14 +0000

    Tweak spec tests

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,23 +1,23 @@
 # frozen_string_literal: true
 
-RSpec.describe "Homes" do
-  describe "GET /show" do
+RSpec.describe "Home" do
+  describe "GET show" do
     it "returns http success" do
-      get "/home/show"
+      get home_path
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /edit" do
+  describe "GET edit" do
     it "returns http success" do
-      get "/home/edit"
+      get edit_home_path
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /update" do
+  describe "PUT update" do
     it "returns http success" do
-      get "/home/update"
+      put home_path
       expect(response).to have_http_status(:success)
     end
   end
