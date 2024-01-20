8f7fe5045a49f78964ff3325b03e1f1768e39a3b
Rick Selby <rick@selby-family.co.uk>
2024-01-20 11:21:24 +0000

    Use full key when looking up text

--- app/helpers/page_text_helper.rb
+++ app/helpers/page_text_helper.rb
@@ -3,6 +3,7 @@
 # Helpers around editable page text
 module PageTextHelper
   def editable_text(key, classes: [])
-    EditableTextHelper.new(self, key, classes:).html
+    full_key = "#{@virtual_path.gsub(%r{/_?}, ".")}.#{key}" # rubocop:disable Rails/HelperInstanceVariable
+    EditableTextHelper.new(self, full_key, classes:).html
   end
 end
