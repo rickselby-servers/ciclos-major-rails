0bf48e243f50e2d79ea84c30a0a3e5c8a7f913e4
Rick Selby <rick@selby-family.co.uk>
2024-02-10 21:45:51 +0000

    Fix page text update spec test

--- spec/system/update_page_text_spec.rb
+++ spec/system/update_page_text_spec.rb
@@ -7,11 +7,11 @@ RSpec.describe "updating page text" do
     sign_in Admin.create
     visit root_path
 
-    page.find("p#pages\\.show\\.block").send_keys "foo"
+    page.find("p#pages\\.home\\.block").send_keys "foo"
 
     click_on "Update Page text"
 
     expect(page).to have_text "foo"
-    expect(PageText.find_by(key: "pages.show.block").text).to eq "pages.show.blockfoo"
+    expect(PageText.find_by(key: "pages.home.block").text).to eq "pages.home.blockfoo"
   end
 end
