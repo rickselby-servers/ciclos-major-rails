90489d0d027f6c06df272d3fecf0a886d3929178
Rick Selby <rick@selby-family.co.uk>
2024-02-10 08:44:57 +0000

    Add model tests

--- app/models/page_text.rb
+++ app/models/page_text.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class PageText < ApplicationRecord
+  validates :key, presence: true, uniqueness: true
 end
--- spec/models/page_text_spec.rb
+++ spec/models/page_text_spec.rb
@@ -3,5 +3,6 @@
 require "rails_helper"
 
 RSpec.describe PageText do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of :key }
+  it { is_expected.to validate_uniqueness_of :key }
 end
