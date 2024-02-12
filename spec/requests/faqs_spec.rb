d0e9e63534f7de354d6b7b8b569bba9c0910d6a6
Rick Selby <rick@selby-family.co.uk>
2024-02-12 21:31:04 +0000

    Start refactoring tests

--- spec/requests/faqs_spec.rb
+++ spec/requests/faqs_spec.rb
@@ -3,6 +3,7 @@
 require "rails_helper"
 
 RSpec.describe "/faqs" do
+  let(:faq) { Faq.create! valid_attributes }
   let(:invalid_attributes) { { question: "", answer: "" } }
   let(:valid_attributes) { { question: "foo", answer: "bar" } }
 
@@ -11,145 +12,137 @@ RSpec.describe "/faqs" do
   end
 
   describe "GET /index" do
-    it "renders a successful response" do
-      Faq.create! valid_attributes
+    subject do
       get faqs_url
-      expect(response).to be_successful
+      response
     end
+
+    it { is_expected.to be_successful }
   end
 
   describe "GET /new" do
+    subject do
+      get new_faq_url
+      response
+    end
+
     context "when not logged in" do
-      it "redirects to the login page" do
-        get new_faq_url
-        expect(response).to redirect_to new_admin_session_path
-      end
+      it { is_expected.to redirect_to new_admin_session_path }
     end
 
     context "when logged in", :logged_in do
-      it "renders a successful response" do
-        get new_faq_url
-        expect(response).to be_successful
-      end
+      it { is_expected.to be_successful }
     end
   end
 
   describe "GET /edit" do
+    subject do
+      get edit_faq_url faq
+      response
+    end
+
     context "when not logged in" do
-      it "redirects to the login page" do
-        faq = Faq.create! valid_attributes
-        get edit_faq_url(faq)
-        expect(response).to redirect_to new_admin_session_path
-      end
+      it { is_expected.to redirect_to new_admin_session_path }
     end
 
     context "when logged in", :logged_in do
-      it "renders a successful response" do
-        faq = Faq.create! valid_attributes
-        get edit_faq_url(faq)
-        expect(response).to be_successful
-      end
+      it { is_expected.to be_successful }
     end
   end
 
   describe "POST /create" do
+    subject(:do_post) do
+      post faqs_url, params: { faq: valid_attributes }
+      response
+    end
+
     context "when not logged in" do
-      it "redirects to the login page" do
-        post faqs_url, params: { faq: valid_attributes }
-        expect(response).to redirect_to new_admin_session_path
-      end
+      it { is_expected.to redirect_to new_admin_session_path }
     end
 
     context "when logged in", :logged_in do
       context "with valid parameters" do
         it "creates a new Faq" do
-          expect do
-            post faqs_url, params: { faq: valid_attributes }
-          end.to change(Faq, :count).by(1)
+          expect { do_post }.to change(Faq, :count).by(1)
         end
 
-        it "redirects to the created faq" do
-          post faqs_url, params: { faq: valid_attributes }
-          expect(response).to redirect_to(faqs_url)
-        end
+        it { is_expected.to redirect_to faqs_url }
       end
 
       context "with invalid parameters" do
-        it "does not create a new Faq" do
-          expect do
-            post faqs_url, params: { faq: invalid_attributes }
-          end.not_to change(Faq, :count)
+        subject(:do_post) do
+          post faqs_url, params: { faq: invalid_attributes }
+          response
         end
 
-        it "renders a response with 422 status (i.e. to display the 'new' template)" do
-          post faqs_url, params: { faq: invalid_attributes }
-          expect(response).to have_http_status(:unprocessable_entity)
+        it "does not create a new Faq" do
+          expect { do_post }.not_to change(Faq, :count)
         end
+
+        it { is_expected.to have_http_status :unprocessable_entity }
       end
     end
   end
 
   describe "PATCH /update" do
+    subject(:do_patch) do
+      patch faq_url(faq), params: { faq: new_attributes }
+      response
+    end
+
     let(:new_attributes) { { question: "bar", answer: "baz" } }
 
     context "when not logged in" do
-      it "redirects to the login page" do
-        faq = Faq.create! valid_attributes
-        patch faq_url(faq), params: { faq: new_attributes }
-        expect(response).to redirect_to new_admin_session_path
-      end
+      it { is_expected.to redirect_to new_admin_session_path }
     end
 
     context "when logged in", :logged_in do
       context "with valid parameters" do
-        let(:new_attributes_string_keys) { new_attributes.transform_keys(&:to_s) }
+        let(:expected_attributes) { new_attributes.transform_keys(&:to_s) }
 
         it "updates the requested faq" do
-          faq = Faq.create! valid_attributes
-          patch faq_url(faq), params: { faq: new_attributes }
-          faq.reload
-          expect(faq.attributes.slice(*new_attributes_string_keys.keys)).to eq new_attributes_string_keys
+          do_patch
+          expect(faq.reload.attributes.slice(*expected_attributes.keys)).to eq expected_attributes
         end
 
-        it "redirects to the faq" do
-          faq = Faq.create! valid_attributes
-          patch faq_url(faq), params: { faq: new_attributes }
-          faq.reload
-          expect(response).to redirect_to(faqs_path)
-        end
+        it { is_expected.to redirect_to faqs_url }
       end
 
       context "with invalid parameters" do
-        it "renders a response with 422 status (i.e. to display the 'edit' template)" do
-          faq = Faq.create! valid_attributes
+        subject(:do_patch) do
           patch faq_url(faq), params: { faq: invalid_attributes }
-          expect(response).to have_http_status(:unprocessable_entity)
+          response
         end
+
+        let(:expected_attributes) { valid_attributes.transform_keys(&:to_s) }
+
+        it "does not update the requested faq" do
+          do_patch
+          expect(faq.attributes.slice(*expected_attributes.keys)).to eq expected_attributes
+        end
+
+        it { is_expected.to have_http_status :unprocessable_entity }
       end
     end
   end
 
   describe "DELETE /destroy" do
+    subject(:do_delete) do
+      delete faq_url faq
+      response
+    end
+
     context "when not logged in" do
-      it "redirects to the login page" do
-        get new_faq_url
-        expect(response).to redirect_to new_admin_session_path
-      end
+      it { is_expected.to redirect_to new_admin_session_path }
     end
 
     context "when logged in", :logged_in do
       it "destroys the requested faq" do
-        faq = Faq.create! valid_attributes
-        expect do
-          delete faq_url(faq)
-        end.to change(Faq, :count).by(-1)
+        faq # load the FAQ before the check that we delete it
+        expect { do_delete }.to change(Faq, :count).by(-1)
       end
 
-      it "redirects to the faqs list" do
-        faq = Faq.create! valid_attributes
-        delete faq_url(faq)
-        expect(response).to redirect_to(faqs_url)
-      end
+      it { is_expected.to redirect_to faqs_url }
     end
   end
 end
