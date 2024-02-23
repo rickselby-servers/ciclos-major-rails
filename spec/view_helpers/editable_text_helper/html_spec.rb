31dfdeebd394c814f1f24307946aacf2dd0088b7
Rick Selby <rick@selby-family.co.uk>
2024-02-23 15:00:55 +0000

    Update spec tests

--- spec/view_helpers/editable_text_helper/html_spec.rb
+++ spec/view_helpers/editable_text_helper/html_spec.rb
@@ -9,14 +9,14 @@ RSpec.describe EditableTextHelper, ".html" do
   let(:key) { "foo.bar" }
   let(:text) { key }
 
-  it { is_expected.to have_css "p#foo\\.bar", text: }
+  it { is_expected.to have_css "div#foo\\.bar", text: }
 
   context "with some content" do
     before { PageText.create key:, text: }
 
     let(:text) { "foo bar baz" }
 
-    it { is_expected.to have_css "p#foo\\.bar", text: }
+    it { is_expected.to have_css "div#foo\\.bar", text: }
   end
 
   context "with HTML" do
@@ -25,14 +25,14 @@ RSpec.describe EditableTextHelper, ".html" do
     let(:text) { "foo bar baz" }
     let(:text_html) { "<strong>#{text}</strong>" }
 
-    it { is_expected.to have_css "p#foo\\.bar strong", text: }
+    it { is_expected.to have_css "div#foo\\.bar strong", text: }
   end
 
   context "with given classes" do
     let(:classes) { %i[foo bar] }
 
-    it { is_expected.to have_css "p.foo", text: }
-    it { is_expected.to have_css "p.bar", text: }
+    it { is_expected.to have_css "div.foo", text: }
+    it { is_expected.to have_css "div.bar", text: }
   end
 
   context "when logged in" do
@@ -43,7 +43,7 @@ RSpec.describe EditableTextHelper, ".html" do
     describe "the form contents" do
       subject { Capybara.string(html).find "form" }
 
-      it { is_expected.to have_css "p#foo\\.bar", text: }
+      it { is_expected.to have_css "div#foo\\.bar", text: }
       it { is_expected.to have_button "Update", visible: :hidden }
     end
   end
