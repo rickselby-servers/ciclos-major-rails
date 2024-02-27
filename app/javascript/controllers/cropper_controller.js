a038d40d8d547a9635a58b0bb4c2f0738cac0926
Rick Selby <rick@selby-family.co.uk>
2024-02-27 22:14:15 +0000

    Add modal and refactor controller

--- app/javascript/controllers/cropper_controller.js
+++ app/javascript/controllers/cropper_controller.js
@@ -3,32 +3,57 @@ import Cropper from "cropperjs"
 
 // Connects to data-controller="cropper"
 export default class extends Controller {
+  initialize() {
+    this.imageInput = this.element.querySelectorAll("[data-cropper-upload]")[0];
+    this.croppableImage = this.element.querySelectorAll("[data-cropper-image]")[0];
+    this.dataInput = this.element.querySelectorAll("[data-cropper-data]")[0];
+    this.cancelButton = this.element.querySelectorAll("[data-cropper-cancel]")[0];
+    this.previews = this.element.querySelectorAll(".previews");
+    this.cropper = null;
+  }
+
   connect() {
-    let inputImage = this.element.querySelectorAll("[data-cropper-upload]")[0],
-        croppableImage = this.element.querySelectorAll("[data-cropper-image]")[0],
-        hiddenInput = this.element.querySelectorAll("[data-cropper-data]")[0];
-    let cropper;
+    this.imageInput.onchange = this.setup.bind(this);
+    this.cancelButton.onclick = this.cancel;
+  }
 
-    inputImage.onchange = function () {
-      if (this.files && this.files.length) {
-        let file = this.files[0];
+  disconnect() {
+    this.destroy();
+  }
 
-        if (/^image\/\w+/.test(file.type)) {
-          croppableImage.src = URL.createObjectURL(file);
+  setup(event) {
+    if (event.target.files && event.target.files.length) {
+      let file = event.target.files[0];
 
-          if (cropper) {
-            cropper.destroy();
-          }
-
-          cropper = new Cropper(croppableImage, {
-            crop: function (event) {
-              hiddenInput.value = JSON.stringify(event.detail)
-            }
-          });
-        } else {
-          window.alert("Please choose an image file.");
-        }
+      if (/^image\/\w+/.test(file.type)) {
+        this.croppableImage.src = URL.createObjectURL(file);
+        this.create();
+      } else {
+        window.alert("Please choose an image file.");
       }
+    }
+  }
+
+  create() {
+    this.destroy();
+    this.cropper = new Cropper(this.croppableImage, {
+      preview: this.previews,
+      crop: this.crop.bind(this)
+    });
+  }
+
+  crop(event) {
+    this.dataInput.value = JSON.stringify(event.detail)
+  }
+
+  cancel() {
+    this.imageInput.value = null;
+    this.croppableImage.src = "";
+  }
+
+  destroy() {
+    if (this.cropper) {
+      this.cropper.destroy();
     }
   }
 }
