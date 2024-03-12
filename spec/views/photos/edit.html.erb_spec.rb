b17d64d80ff2727af78d2d4aa161dd95c0d6cf2a
Rick Selby <rick@selby-family.co.uk>
2024-03-12 21:26:06 +0000

    Update page specs

--- spec/views/photos/edit.html.erb_spec.rb
+++ spec/views/photos/edit.html.erb_spec.rb
@@ -7,13 +7,18 @@ RSpec.describe "photos/edit" do
 
   before { assign :photo, photo }
 
-  it { is_expected.to have_link href: photos_path }
   it { is_expected.to have_css "form[action='#{photo_path(photo)}'][method='post']" }
 
-  context "with the form" do
-    subject { page.find("form") }
+  context "with the edit form" do
+    subject { page.find("form##{dom_id(photo, :edit_form)}") }
 
     it { is_expected.to have_field "photo[alt]" }
+    it { is_expected.to have_button "commit" }
+  end
+
+  context "with the delete form" do
+    subject { page.find("form##{dom_id(photo, :delete_form)}") }
+
     it { is_expected.to have_button "commit" }
   end
 end
