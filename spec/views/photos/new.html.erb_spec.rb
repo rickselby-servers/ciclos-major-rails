6a09aeb58a59217698bfc423784bec62cc9beb7c
Rick Selby <rick@selby-family.co.uk>
2024-03-21 22:47:14 +0000

    Tweak new photos modal and update spec

--- spec/views/photos/new.html.erb_spec.rb
+++ spec/views/photos/new.html.erb_spec.rb
@@ -4,15 +4,19 @@ require "rails_helper"
 
 RSpec.describe "photos/new" do
   let(:photo) { Photo.new }
+  let(:target) { create :gallery }
 
-  before { assign :photo, photo }
+  before do
+    assign :photo, photo
+    assign :target, target
+  end
 
   it { is_expected.to have_css "form[action='#{photos_path}'][method='post']" }
 
   context "with the form" do
     subject { page.find("form") }
 
-    it { is_expected.to have_field "photo[alt]" }
+    # TODO: has photos field?
     it { is_expected.to have_button "commit" }
   end
 end
