0e954f55be2d91ef0a26fb4bfe17acc35cd80c2a
Rick Selby <rick@selby-family.co.uk>
2024-05-03 22:16:17 +0100

    Refactor image resizing examples

--- spec/models/shared/resize_image.rb
+++ spec/models/shared/resize_image.rb
@@ -5,27 +5,21 @@ require "vips"
 RSpec.shared_examples "it resizes an image correctly" do |key, sizes|
   let(:model) { create described_class.model_name.i18n_key, key => image }
   let(:image_path) { ActiveStorage::Blob.service.path_for model.public_send(key).key }
-  let(:vips_image) { Vips::Image.new_from_file image_path }
 
-  let(:image) { Rack::Test::UploadedFile.new "spec/fixtures/files/#{basename}.jpg", "image/jpg" }
-
-  sizes.each do |file_basename, size|
-    context "with the file #{file_basename}" do
-      let(:basename) { file_basename }
-
-      example { expect(vips_image.width).to eq size[0] }
-      example { expect(vips_image.height).to eq size[1] }
-    end
-  end
+  it_behaves_like "it resizes an image", sizes
 end
 
 RSpec.shared_examples "it resizes a photo correctly" do |sizes|
   let(:model) { create described_class.model_name.i18n_key }
   let(:photo) { model.photos.create photo: image }
   let(:image_path) { ActiveStorage::Blob.service.path_for photo.photo.key }
-  let(:vips_image) { Vips::Image.new_from_file image_path }
 
+  it_behaves_like "it resizes an image", sizes
+end
+
+RSpec.shared_examples "it resizes an image" do |sizes|
   let(:image) { Rack::Test::UploadedFile.new "spec/fixtures/files/#{basename}.jpg", "image/jpg" }
+  let(:vips_image) { Vips::Image.new_from_file image_path }
 
   sizes.each do |file_basename, size|
     context "with the file #{file_basename}" do
