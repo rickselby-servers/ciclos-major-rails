a1fdcedeb80f92e49e172dd74f95a0590f1ab2a2
Rick Selby <rick@selby-family.co.uk>
2024-05-03 22:05:40 +0100

    Set up failing tests for resizing photos

--- spec/models/carousel_spec.rb
+++ spec/models/carousel_spec.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 require "rails_helper"
+require_relative "shared/resize_image"
 
 RSpec.describe Carousel do
   it { is_expected.to validate_presence_of :reference }
 
   it { is_expected.to be_audited }
   it { is_expected.to have_many :photos }
+
+  it_behaves_like "it resizes a photo correctly",
+                  { "400x400" => [400, 400], "400x800" => [200, 400], "800x400" => [800, 400] }
 end
--- spec/models/shared/resize_image.rb
+++ spec/models/shared/resize_image.rb
@@ -18,3 +18,21 @@ RSpec.shared_examples "it resizes an image correctly" do |key, sizes|
     end
   end
 end
+
+RSpec.shared_examples "it resizes a photo correctly" do |sizes|
+  let(:model) { create described_class.model_name.i18n_key }
+  let(:photo) { model.photos.create photo: image }
+  let(:image_path) { ActiveStorage::Blob.service.path_for photo.photo.key }
+  let(:vips_image) { Vips::Image.new_from_file image_path }
+
+  let(:image) { Rack::Test::UploadedFile.new "spec/fixtures/files/#{basename}.jpg", "image/jpg" }
+
+  sizes.each do |file_basename, size|
+    context "with the file #{file_basename}" do
+      let(:basename) { file_basename }
+
+      example { expect(vips_image.width).to eq size[0] }
+      example { expect(vips_image.height).to eq size[1] }
+    end
+  end
+end
